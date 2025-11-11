8af0332598b83aa5ad82c6d402df635178b6511c
bindu1206 <bindui1206@gmail.com>
2025-11-11 19:45:00 +0530

    Creating a Redux Store

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,23 @@ import { Navbar } from "./components/layout/Navbar";
 import { Landing } from "./components/layout/Landing";
 import { Login } from "./components/auth/Login";
 import { Register } from "./components/auth/Register";
+// Redux
+import { Provider } from "react-redux";
+import  store  from "./store";
 
 const App = () => (
-  <BrowserRouter>
-    <>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Landing />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/register" element={<Register />} />
-      </Routes>
-    </>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+        </Routes>
+      </>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
