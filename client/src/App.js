c85c5e571a179b6cf959977b410db41d9da85d44
bindu1206 <bindui1206@gmail.com>
2025-11-11 13:59:25 +0530

    React Router Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/layout/Navbar";
 import { Landing } from "./components/layout/Landing";
+import { Login } from "./components/auth/Login";
+import { Register } from "./components/auth/Register";
 
 const App = () => (
-  <>
-    <Navbar />
-    <Landing />
-  </>
+  <BrowserRouter>
+    <>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
+      </Routes>
+    </>
+  </BrowserRouter>
 );
 
 export default App;
