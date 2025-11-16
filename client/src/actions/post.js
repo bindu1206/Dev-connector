ca3b5334a60caee8c42318f0756b31c18a960eb0
bindu1206 <bindui1206@gmail.com>
2025-11-16 12:55:08 +0530

    Post Item Component, Like and Unlike Component

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,4 +1,4 @@
-import { GET_POSTS, POST_ERROR } from "./types";
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types";
 import axios from "axios";
 import { setAlert } from "./alert";
 
@@ -10,6 +10,46 @@ export const getPosts = () => async (dispatch) => {
     dispatch({
       type: GET_POSTS,
       payload: res.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
+
+// Add like
+export const addLike = (id) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/like/${id}`);// postId
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: res.data},// id to update that post in posts
+    });
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
+
+// Remove like
+export const removeLike = (id) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/unlike/${id}`);// postId
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: res.data},// id to update that post in posts
     });
   } catch (error) {
     dispatch({
--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
 import { getPosts } from '../../actions/post'
+import PostItem from './PostItem'
  
 const Posts = ({ getPosts, post: { posts, loading } }) => {
     useEffect(() => {
@@ -10,7 +11,21 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
     }, [getPosts])
 
   return (
-    <div>Posts</div>
+    <>
+      <h1 className="large text-primary">Posts</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Welcome to the community
+      </p>
+      {/* Post form */}
+      <div className="posts">
+        {loading ? <Spinner /> : 
+          posts.map((post) => (
+            <PostItem key={post._id} post={post} />
+          ))
+        }
+        
+      </div>
+    </>
   )
 }
 
